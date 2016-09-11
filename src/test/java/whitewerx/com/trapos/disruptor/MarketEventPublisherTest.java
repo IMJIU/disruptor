2e3b14f6be21722afea272bede580e5352d6c94a
zlfkyo@sina.com <zlfkyo@sina.com>
2016-09-12 01:16:50 +0800

    j

--- src/test/java/whitewerx/com/trapos/disruptor/MarketEventPublisherTest.java
+++ src/test/java/whitewerx/com/trapos/disruptor/MarketEventPublisherTest.java
@@ -10,14 +10,10 @@ import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.EventFactory;
 import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
-import com.lmax.disruptor.SingleThreadedClaimStrategy;
-import com.lmax.disruptor.WaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
-
 import whitewerx.com.trapos.gateway.TextMessageSubscriber;
 import whitewerx.com.trapos.test.LongEvent;
 import whitewerx.com.trapos.test.LongEventFactory;
@@ -37,7 +33,8 @@ public class MarketEventPublisherTest {
 		final String delimitedMessage = "T|B|5.1t|R|EURUSD|1.3124";
 		final MarketEvent emptyEvent = context.mock(MarketEvent.class);
 		@SuppressWarnings("unchecked")
-		final RingBufferAdapter<MarketEvent> ringBuffer = (RingBufferAdapter<MarketEvent>) context.mock(RingBufferAdapter.class);
+		final RingBufferAdapter<MarketEvent> ringBuffer = (RingBufferAdapter<MarketEvent>) context
+				.mock(RingBufferAdapter.class);
 
 		TextMessageSubscriber publisher = new MarketEventPublisher(ringBuffer);
 		context.checking(new Expectations() {
@@ -59,27 +56,13 @@ public class MarketEventPublisherTest {
 		publisher.accept(delimitedMessage);
 	}
 
-	@Test
-	public void publishEvent2() {
-		final String delimitedMessage = "T|B|5.1t|R|EURUSD|1.3124";
-		final MarketEvent emptyEvent = context.mock(MarketEvent.class);
-		@SuppressWarnings("unchecked")
-		// final RingBufferAdapter<MarketEvent> ringBuffer = new
-		// RingBufferAdapter<>(new RingBuffer<>(eventFactory, bufferSize))
-		RingBuffer<MarketEvent> ringBuffer = new RingBuffer<MarketEvent>(MarketEvent.FACTORY, new SingleThreadedClaimStrategy(16), new BlockingWaitStrategy());
-		MarketEventPublisher eventPublisher = new MarketEventPublisher(new RingBufferAdapter<MarketEvent>(ringBuffer));
-		ringBuffer.next();
-		ringBuffer.get(1);
-		ringBuffer.setGatingSequences();
-		ringBuffer.publish(1);
-		eventPublisher.accept(delimitedMessage);
-	}
+	
 
 	@Test
-	public void publishEvent3() {
+	public void publishEvent_1000000Cnt() {
 		EventFactory<LongEvent> eventFactory = new LongEventFactory();
-		ExecutorService executor = Executors.newSingleThreadExecutor();
-		int ringBufferSize = 1024 * 1024; // RingBuffer 大小，必须是 2 的 N 次方；
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		int ringBufferSize =  4 * 1024 * 1024; // RingBuffer 大小，必须是 2 的 N 次方；
 
 		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(eventFactory, ringBufferSize, executor);
 
@@ -89,13 +72,18 @@ public class MarketEventPublisherTest {
 		disruptor.start();
 		// 发布事件；
 		RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
-		long sequence = ringBuffer.next();// 请求下一个事件序号；
 
-		LongEvent event = ringBuffer.get(sequence);// 获取该序号对应的事件对象；
-		long data = 1;// 获取要通过事件传递的业务数据；
-		event.set(data);
-
-		ringBuffer.publish(sequence);// 发布事件；
+		int countOfEvents = 10000000;
+		long startTime = System.nanoTime();
+		for (int i = 1; i <= countOfEvents; i++) {
+			long sequence = ringBuffer.next();// 请求下一个事件序号；
+			LongEvent event = ringBuffer.get(sequence);// 获取该序号对应的事件对象；
+			long data = 1;// 获取要通过事件传递的业务数据；
+			event.set(data);
+			ringBuffer.publish(sequence);// 发布事件；
+		}
+		long time = System.nanoTime() - startTime;
+		System.out.printf("Took  %.3f seconds to send %d int events", time / 1e9, countOfEvents);
 
 	}
 
